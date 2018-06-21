c792e20b1fe78b68e5567d64cf96eb18a565bc35
Alexandre Quemy <alexandre.quemy@gmail.com>
2018-06-21 10:06:14 +0200

    Improve documentation

--- echr-scraping/get_documents.py
+++ echr-scraping/get_documents.py
@@ -5,28 +5,37 @@ import json
 import os
 import shutil
 
-base_url = "http://hudoc.echr.coe.int/app/conversion/docx/?library=ECHR&filename=please_give_me_the_document.docx&id="
-perma_url = "http://hudoc.echr.coe.int/eng?i="
+BASE_URL = "http://hudoc.echr.coe.int/app/conversion/docx/?library=ECHR&filename=please_give_me_the_document.docx&id="
+PERMA_URL = "http://hudoc.echr.coe.int/eng?i="
 
 def get_documents(id_list, folder, update):
+    """Get documents according to the specified list
+
+        :param id_list: list of document id
+        :type id_list: [str]
+        :param folder: path where to save the documents
+        :type folder: str
+        :param update: overwrite existing documents
+        :type: bool
+    """
     for i, doc_id in enumerate(id_list):
-        print("Document {}/{}: {}".format(i, len(id_list), doc_id))
+        print(" - Document {}/{}: {}".format(i, len(id_list), doc_id))
         filename = "%s.docx"%(doc_id.strip())
         filename = os.path.join(folder, filename)
         if update and not os.path.isfile(filename):
-            url = base_url + doc_id.strip()
+            url = BASE_URL + doc_id.strip()
             r = requests.get(url, stream=True)
             if not r.ok:
-                print("Failed to fetch document %s"%(doc_id))
-                print("URL: %s"%(url))
-                print("Permalink: %s"%(perma_url + doc_id.strip()))
+                print("\tFailed to fetch document %s"%(doc_id))
+                print("\tURL: %s"%(url))
+                print("\tPermalink: %s"%(PERMA_URL + doc_id.strip()))
                 continue
             with open(filename, 'wb') as f:
                 for block in r.iter_content(1024):
                     f.write(block)
-            print("request complete, see %s"%(filename))
+            print("\tRequest complete, see %s"%(filename))
         else:
-            print("skip as document exists already")
+            print("\tSkip as document exists already")
 
 def main(args):
     id_list = []
@@ -48,6 +57,7 @@ def main(args):
             print(e)
             exit(1)
 
+    print("# Get documents from HUDOC")
     get_documents(id_list, args.output_folder, args.u)
 
 def parse_args(parser):
@@ -58,8 +68,8 @@ def parse_args(parser):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Filter and format ECHR cases information')
-    parser.add_argument('--input_file', type=str, default="./cases_info/raw_cases_info.json")
-    parser.add_argument('--output_folder', type=str, default="./raw_documents")
+    parser.add_argument('--input_file', type=str, default="./build/echr_database/cases_info/raw_cases_info.json")
+    parser.add_argument('--output_folder', type=str, default="./build/echr_database/raw_documents")
     parser.add_argument('-f', action='store_true')
     parser.add_argument('-u', action='store_true')
     args = parse_args(parser)
