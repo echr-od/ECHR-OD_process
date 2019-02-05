c9feaacd972fc388fad2e017c6763c8d744d4136
Alexandre Quemy <alexandre.quemy@gmail.com>
2019-02-05 17:18:23 +0100

    Setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='echr_process',
+    version='1.0.2',
+    description='European Court of Human Rights OpenData construction process',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/aquemy/ECHR-OD_process',  # Optional
+    author='Alexandre Quemy',
+    author_email='aquemy@pl.ibm.com',  # Optional
+    classifiers=[ 
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='classification justice prediction',
+    install_requires=requirements,
+    project_urls={ 
+        'Bug Reports': 'https://github.com/aquemy/ECHR-OD_process/issues',
+        'Say Thanks!': 'https://saythanks.io/to/aquemy',
+        'Source': 'https://github.com/aquemy/ECHR-OD_process',
+    },
+)
