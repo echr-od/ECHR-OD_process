91132afddec314a533aee12783ea0fa7782632c3
Alexandre Quemy <alexandre.quemy@gmail.com>
2020-10-24 23:28:57 +0200

    Separate the NLP models and datasets from the database generation

--- echr/utils/build.py
+++ echr/utils/build.py
@@ -11,6 +11,7 @@ from echr.utils.misc import get_from_path
 
 log = getlogger()
 
+
 def parse_argument(arg, cli_args):
     if arg.startswith('$'):
         var_name = arg[1:]
@@ -36,13 +37,63 @@ def parse_argument(arg, cli_args):
     else:
         return arg, 0
 
+def parse_workflow(console, steps_names, workflow, workflow_path, actions_path):
+    parsed_workflow = []
+    parsed_workflow_steps_names = steps_names
+    available_actions = [f.split('.')[0] for f in os.listdir(actions_path) if
+                         os.path.splitext(f)[1] in ['.yaml', '.yml']]
+    available_workflows = [f.split('.')[0] for f in os.listdir(workflow_path) if
+                           os.path.splitext(f)[1] in ['.yaml', '.yml']]
+    for e in workflow.get('steps', []):
+        print(e)
+        if e.get('type') == 'action':
+            action_name = e.get('run')
+            if action_name not in available_actions:
+                console.print(TAB + '  ⮡ [bold red]:double_exclamation_mark: Workflow is invalid. '
+                                    'Action {} does not exist.'.format(action_name))
+                exit(1)
+            if action_name not in parsed_workflow_steps_names:
+                try:
+                    with open(os.path.join(actions_path, '{}.yml'.format(action_name))) as f:
+                        action = yaml.load(f, Loader=yaml.FullLoader)
+                        parsed_workflow.append(action)
+                        parsed_workflow_steps_names.append(action_name)
+                except Exception as e:
+                    console.print_exception()
+                    log.error(e)
+                    console.print(TAB + '  ⮡ [bold red]:double_exclamation_mark: '
+                                        'Could not properly load the action {}!'.format(action_name))
+                    exit(1)
+        elif e.get('type') == 'workflow':
+            workflow_name = e.get('run')
+            if workflow_name not in available_workflows:
+                console.print(TAB + '  ⮡ [bold red]:double_exclamation_mark: Workflow {} does not exist. '
+                                    'Available workflows are: {}'.format(workflow_name, ', '.join(available_workflows)))
+                exit(1)
+            try:
+                with open(os.path.join(workflow_path, '{}.yml'.format(workflow_name))) as f:
+                    new_workflow = yaml.load(f, Loader=yaml.FullLoader)
+                    parsed_workflow += parse_workflow(console, parsed_workflow_steps_names, new_workflow, workflow_path, actions_path)
+            except Exception as e:
+                console.print_exception()
+                log.error(e)
+                console.print(TAB + '  ⮡ [bold red]:double_exclamation_mark: '
+                                    'Could not properly load the workflow {}!'.format(workflow_name))
+                exit(1)
+        else:
+            console.print(TAB + '  ⮡ [bold red]:double_exclamation_mark: Workflow is invalid. '
+                                'Type {} does not exist.'.format(e.get('type')))
+            exit(1)
+    return parsed_workflow
 
 def load_workflow(console, args):
+    WORKFLOW_PATH = './workflows'
+    ACTION_PATH = os.path.join(WORKFLOW_PATH, 'actions')
     workflow = args.workflow
     workflow_steps = []
     console.print(TAB + '> Workflow to execute: [bold black on green]{}'.format(workflow.upper()))
-    if os.path.isdir('./workflows'):
-        available_workflows = [f.split('.')[0] for f in os.listdir('./workflows') if
+    if os.path.isdir(WORKFLOW_PATH):
+        available_workflows = [f.split('.')[0] for f in os.listdir(WORKFLOW_PATH) if
                                os.path.splitext(f)[1] in ['.yaml', '.yml']]
         if workflow not in available_workflows:
             console.print(TAB + '  ⮡ [bold red]:double_exclamation_mark: Workflow {} does not exist. '
@@ -50,7 +101,7 @@ def load_workflow(console, args):
             exit(1)
         else:
             try:
-                with open(os.path.join('./workflows', '{}.yml'.format(workflow))) as f:
+                with open(os.path.join(WORKFLOW_PATH, '{}.yml'.format(workflow))) as f:
                     workflow_steps = yaml.load(f, Loader=yaml.FullLoader)
             except Exception as e:
                 console.print_exception()
@@ -63,14 +114,17 @@ def load_workflow(console, args):
         console.print(TAB + '  ⮡ [bold red]:double_exclamation_mark: Workflow folder does not exist!')
         exit(1)
 
+    console.print(TAB + '> Parse workflow')
+    workflow_steps = parse_workflow(console, [], workflow_steps, WORKFLOW_PATH, ACTION_PATH)
+
     console.print(TAB + '> Replace workflow variables')
     all_replaced = True
     try:
-        for i, step in enumerate(workflow_steps.get('steps', [])):
+        for i, step in enumerate(workflow_steps):
             for arg in step.get('args', {}):
                 val, rc = parse_argument(str(step['args'][arg]), cli_args=args)
                 if rc != 0:
-                    workflow_steps['steps'][i]['args'][arg] = val
+                    workflow_steps[i]['args'][arg] = val
                     if rc < 0:
                         console.print(TAB + '  ⮡ [bold red]:double_exclamation_mark: STEP{}.{}={}'.format(i, arg, val))
                         all_replaced = False
