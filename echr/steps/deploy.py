4d60c3b365769cc3d0748456b3684acdf89b9585
Alexandre Quemy <alexandre.quemy@gmail.com>
2020-10-29 08:34:34 +0100

    Allow to specify the build name in deployment

--- echr/steps/deploy.py
+++ echr/steps/deploy.py
@@ -75,9 +75,12 @@ def runner(params_str, build, detach, force, update):
     print(TAB + "> Fetch and rebase the repository... [green][DONE]")
 
     print(TAB + "> Run workflow and detach... [green][DONE]")
+    build_str = ""
+    if 'build' in params:
+        build_str = "--build {}".format(params['build'])
     cmd = 'tmux new -A -s echr -d "docker run -ti ' \
           '--mount src={},dst=/tmp/echr_process/,type=bind ' \
-          'echr_build build --workflow {}"'.format(REPO_PATH, params['workflow'])
+          'echr_build build --workflow {} {}"'.format(REPO_PATH, params['workflow'], build_str)
     stdin, stdout, stderr = client.exec_command(cmd)
 
 
