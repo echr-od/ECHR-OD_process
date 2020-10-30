5bb9c1d32bfda97f911bd26b52398590e7a3f16e
Alexandre Quemy <alexandre.quemy@gmail.com>
2020-10-30 12:12:32 +0100

    Fix the argument parser

--- build.py
+++ build.py
@@ -104,7 +104,8 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--force', action='store_true')
     parser.add_argument('-s', '--strict', action='store_true')
     parser.add_argument('--no-tty', action='store_true')
-    parser.add_argument('--upgrade_endpoint',  type=str, default='', action='store_true')
+    parser.add_argument('--upgrade_endpoint',  type=str, default='', help='Endpoint to upgrade '
+                                                                          'an instance of the explorer')
     parser.add_argument('--max_documents', type=int, help='Maximum number of documents to retrieve')
     parser.add_argument('--params', type=str, help='Additional parameters to override workflow parameters')
     parser.add_argument('-w', '--workflow', type=str, default='local', help='Workflow to execute')
