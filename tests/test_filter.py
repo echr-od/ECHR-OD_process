6790e807d085c78032c41789ed6287030381a9be
Alexandre Quemy <alexandre.quemy@gmail.com>
2020-12-17 19:03:51 +0100

    Add unit tests for conclusion parser #125

---
+++ tests/test_filter.py
@@ -0,0 +1,46 @@
+from mock import patch
+from unittest.mock import MagicMock
+import json
+import os
+from rich.console import Console
+
+from echr.steps.filter import format_conclusion, find_base_articles, split_and_format_article
+from echr.utils.misc import compare_two_lists
+
+class TestFormatConclusion:
+
+    def test_split_and_format_article(self):
+        article_examples = [
+            {'input': '3', 'output': ['3']},
+            {'input': '5-1+6+7+p1-3', 'output': ['5-1', '6', '7', 'p1-3']},
+            {'input': '5-1+5-3', 'output': ['5-1', '5-3']},
+            {'input': '14+5-3', 'output': ['14', '5-3']},
+        ]
+
+        for e in article_examples:
+            res = split_and_format_article(article=e['input'])
+            print(res, e['output'])
+            assert (sorted(res) == sorted(e['output']))
+
+    def test_find_base_articles(self):
+        base_article_examples = [
+            {'input': ['3'], 'output': ['3']},
+            {'input': ['5', '6', '7', 'p1-3'], 'output': ['5', '6', '7', 'p1-3']},
+            {'input': ['5-1', '5-3'], 'output': ['5', '5']},
+            {'input': ['14+5-3'], 'output': ['14']},
+            {'input': ['p4-4'], 'output': ['p4-4']}
+        ]
+
+        for e in base_article_examples:
+            res = find_base_articles(articles=e['input'])
+            print(res, e['output'])
+            assert (sorted(res) == sorted(e['output']))
+
+    def test_format_conclusion(self):
+        with open('tests/data/sample_format_conclusion.json') as json_file:
+            conclusions_examples = json.load(json_file)
+
+        for e in conclusions_examples:
+            res = format_conclusion(e['input'])
+            assert (compare_two_lists(res, e['output']))
+
