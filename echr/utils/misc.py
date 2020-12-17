d5d366ec746ff9f93b188a43de884679a47c4d0a
Alexandre Quemy <alexandre.quemy@gmail.com>
2020-12-17 19:21:49 +0100

    Add the the misc file #125

--- echr/utils/misc.py
+++ echr/utils/misc.py
@@ -9,3 +9,15 @@ def get_from_dict(data, keys):
 
 def get_from_path(data, path, sep='.'):
     return get_from_dict(data, path.split(sep))
+
+
+def compare_two_lists(list1: list, list2: list) -> bool:
+    """
+    Compare two lists.
+    :param list1: first list.
+    :param list2: second list.
+    :return:      if there is difference between both lists.
+    """
+    diff = [i for i in list1 + list2 if i not in list1 or i not in list2]
+    result = len(diff) == 0
+    return result
