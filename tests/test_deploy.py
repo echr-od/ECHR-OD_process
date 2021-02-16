1bd6c6388c4aed50d6d004f588f41b49cdf162ee
Alexandre Quemy <alexandre.quemy@gmail.com>
2021-02-16 10:52:05 +0100

    Fix some warnings from Codacy

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -24,22 +24,22 @@ class TestRunner:
     @staticmethod
     def test_params_ok():
         params_str = "host=localhost user=foo password=bar " \
-                     "folder=/tmp/ECHR-OD_process/build/ build=echr_update workflow=database"
+                     "folder=example build=echr_update workflow=database"
         ok, params = parse_server_parameters(params_str=params_str)
         assert ok
         assert params == {'host': 'localhost',
                           'user': 'foo',
                           'password': 'bar',
-                          'folder': '/tmp/ECHR-OD_process/build/',
+                          'folder': 'example',
                           'build': 'echr_update',
                           'workflow': 'database'}
 
     @staticmethod
     def test_running_missing_params():
-        rc = runner(params_str="user=foo password=bar", build="/tmp/", detach=False, force=False, update=False)
+        rc = runner(params_str="user=foo password=bar", build="example", detach=False, force=False, update=False)
         assert rc == 2
 
     @staticmethod
     def test_running_exception_params():
-        rc = runner(params_str=None, build="/tmp/", detach=False, force=False, update=False)
+        rc = runner(params_str=None, build="example", detach=False, force=False, update=False)
         assert rc == 11
